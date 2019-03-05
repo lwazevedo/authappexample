7ab4d29a4b066a55702630f438a5bca4df3ac7b7
luiz azevedo <lwnazevedo@gmail.com>
2019-03-05 11:55:29 -0300

    Exmplo da documentação do react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const Public = () => <h3>Public</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false
+  };
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState(() => ({
+        redirectToReferrer: true
+      }));
+    });
+  };
+  render() {
+    const { redirectToReferrer } = this.state;
+    const { from } = this.props.location.state || { from: { pathname: '/' } };
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />;
+    }
+    return (
+      <div>
+        <p>You must log in to view this page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+const AuthButton = withRouter(({ history }) =>
+  fakeAuth.isAuthenticated === true ? (
+    <p>
+      Welcome!{' '}
+      <button
+        onClick={() => {
+          fakeAuth.signout(() => history.push('/'));
+        }}
+      >
+        Sign Out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <AuthButton />
+          <ul>
+            <li>
+              <Link to='/public'>Public Page</Link>
+            </li>
+            <li>
+              <Link to='/protected'>Protected Page</Link>
+            </li>
+          </ul>
+          <Route path='/public' component={Public} />
+          <Route path='/login' component={Login} />
+          <PrivateRoute path='/protected' component={Protected} />
+        </div>
+      </Router>
     );
   }
 }
